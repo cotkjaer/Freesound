3ce89dd27baafcecec02933c527c5008e235ba45
Christian Otkjær <christian@otkjaer.dk>
2019-10-11 16:24:54 +0200

    Initial Commit

---
+++ Freesound/Freesound.h
@@ -0,0 +1,19 @@
+//
+//  Freesound.h
+//  Freesound
+//
+//  Created by Christian Otkjær on 11/10/2019.
+//  Copyright © 2019 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Freesound.
+FOUNDATION_EXPORT double FreesoundVersionNumber;
+
+//! Project version string for Freesound.
+FOUNDATION_EXPORT const unsigned char FreesoundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Freesound/PublicHeader.h>
+
+
